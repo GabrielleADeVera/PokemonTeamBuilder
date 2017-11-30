7278105543a43169d0dc5bf122fc092bb1cca62f
Thomas Desmond <thomasdes533@gmail.com>
2017-11-30 07:20:32 -0800

    Turning YOUR API KEY HERE to string so project will build

--- Week9PrismExampleApp/ApiKeys.cs
+++ Week9PrismExampleApp/ApiKeys.cs
@@ -4,6 +4,6 @@ namespace Week9PrismExampleApp
     public class ApiKeys
     {
 		// Obtain API Key https://openweathermap.org/api
-		public const string WeatherKey = YOUR_API_KEY_HERE;
+		public const string WeatherKey = "YOUR_API_KEY_HERE";
     }
 }
